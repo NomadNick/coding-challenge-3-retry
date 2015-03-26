2e01bf7c0de6259a9aff9543091b9087881fba82
NomadNick <emailnick123@gmail.com>
2015-03-25 19:26:49 -0700

    corrected books push, added comments

--- cf_library.js
+++ cf_library.js
@@ -1,11 +1,16 @@
 function Library(name) {
+	//takes name parameter and uses it to set the name of the instance of Library
 	this.name = name;
+	//an empty array is added for the shelves to be pushed to
 	this.shelves = [];
+	//method to add shelves using the name of the shelf as the parameter and going to the shelves array
 	this.addShelf = function(name) {
 		this.shelves.push(name);
 	};
+	//method to remove the shelves by finding the name of the shelf and assigning it to 'i'
 	this.removeShelf = function(name) {
 		var i = this.shelves.indexOf(name);
+		//'var i' will find the index of the named shelf, but return -1 if it is not found. Therefore, we need to check to make sure it does not equal -1 first, then splice (remove) 'i' from the array and return a new array with that shelf index removed
 		if (i != -1) {
 			this.shelves.splice(i, 1);
 		}
@@ -13,19 +18,25 @@ function Library(name) {
 }
 
 function Shelf(name) {
+	//takes name parameter and uses it to set the name of the instance of Shelf
 	this.name = name;
+	//an empty array is added for the books to be pushed to
 	this.books = [];
+	//method to add books using the name of the book as the parameter and going to the books array
 	this.addBook = function(name) {
 		this.books.push(name);
 	};
+	//method to remove the shelves by finding the name of the shelf and assigning it to 'i'
 	this.removeBook = function(name) {
 		var i = this.books.indexOf(name);
+		//'var i' will find the index of the named book, but return -1 if it is not found. Therefore, we need to check to make sure it does not equal -1 first, then splice (remove) 'i' from the array and return a new array with that book index removed
 		if (i != -1) {
-			this.shelves.splice(i, 1);
+			this.books.splice(i, 1);
 		}
 	};
 }
 
+//constructor notation to allow for easily adding multiple objects
 function Book(bookTitle, authorFirstName, authorLastName) {
 	this.bookTitle = bookTitle;
 	this.authorFirstName = authorFirstName;
